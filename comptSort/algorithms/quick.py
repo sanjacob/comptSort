5c2445240aca5f88541bfab003f6617c713fbc74
jacobszpz <jacobszpz@protonmail.com>
2022-04-06 19:49:23 +0100

    Implement Mergesort, Quicksort, update tests and add char test

--- comptSort/algorithms/quick.py
+++ comptSort/algorithms/quick.py
@@ -17,12 +17,37 @@
 # Also available at https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
 from collections.abc import Sequence
+from random import randrange
 from typing import Any
 
 from .sorter import Sorter, in_order
 
 
 class QuickSort(Sorter):
+  """Swap elements on both sides of a pivot"""
+
   @staticmethod
-  def sort(uData: Sequence[Any], asc: bool = False) -> None:
-    raise NotImplementedError
+  def sort(uData: Sequence[Any], asc: bool = True) -> None:
+    QuickSort._quicksort(uData, 0, len(uData) - 1, asc)
+
+  @classmethod
+  def _partition(cls, a, lo, hi, *, asc: bool = True, random_pivot: bool = True):
+    # Random pivot support
+    pivot_i = randrange(lo, hi) if random_pivot else hi
+    pivot = a[hi]
+    top = lo
+
+    for i in range(lo, hi):
+      if in_order(a[i], pivot, asc):
+        a[i], a[top] = a[top], a[i]
+        top += 1
+
+    a[hi], a[top] = a[top], a[hi]
+    return top
+
+  @classmethod
+  def _quicksort(cls, a, lo, hi, asc: bool = True):
+    if hi > lo:
+      pivot = cls._partition(a, lo, hi, asc=asc)
+      cls._quicksort(a, lo, pivot - 1, asc=asc)
+      cls._quicksort(a, pivot + 1, hi, asc=asc)
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -6,22 +6,35 @@ from comptSort import comptSort
 from hypothesis import assume, example, given
 from hypothesis import strategies as st
 
-unsorted_lists = st.lists(st.integers())
+# Test with lists of integers and characters
+unsorted_lists = st.one_of(st.lists(st.integers()), st.lists(st.characters()))
 
 @given(unsorted_lists, st.booleans())
-def test_bubble(uData: list[Any], asc: bool):
+def test_bubble(uData, asc):
   expect = sorted(uData, reverse=not asc)
   result = comptSort(uData, Sorting.BUBBLE, asc=asc)
   assert result == expect
 
 @given(unsorted_lists, st.booleans())
-def test_insertion(uData: list[Any], asc: bool):
+def test_insertion(uData, asc):
   expect = sorted(uData, reverse=not asc)
   result = comptSort(uData, Sorting.INSERTION, asc=asc)
   assert result == expect
 
 @given(unsorted_lists, st.booleans())
-def test_binary_insertion(uData: list[Any], asc: bool):
+def test_binary_insertion(uData, asc):
   expect = sorted(uData, reverse=not asc)
   result = comptSort(uData, Sorting.BIN_INSERTION, asc=asc)
+  assert result == expect
+
+@given(unsorted_lists, st.booleans())
+def test_merge(uData, asc):
+  expect = sorted(uData, reverse=not asc)
+  result = comptSort(uData, Sorting.MERGE, asc=asc)
+  assert result == expect
+
+@given(unsorted_lists, st.booleans())
+def test_quick(uData, asc):
+  expect = sorted(uData, reverse=not asc)
+  result = comptSort(uData, Sorting.QUICK, asc=asc)
   assert result == expect
