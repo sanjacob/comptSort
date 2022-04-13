cb4309d2a0992e77ff80391ca3106b12dfbd5056
jacobszpz <jacobszpz@protonmail.com>
2022-04-13 20:16:09 +0100

    Add CLI script

---
+++ comptSort/cli.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+"""comptSort CLI module."""
+
+
+import argparse
+from typing import Sequence
+
+from . import comptSort
+
+algorithms = ['bubble', 'insertion', 'bin_insertion', 'merge', 'quick']
+
+
+def _from_file(filename: str, as_ints: bool = False):
+    """Read items from file, each per line."""
+    items = []
+
+    with open(filename) as f:
+        for line in f:
+            item = line.strip()
+            if as_ints:
+                item = int(item)
+            items.append(item)
+
+    return items
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    """Sorting Function comptSort Command Line Interface."""
+    parser = argparse.ArgumentParser(
+        'comptSort', description='Test the comptSort library by sorting lists.')
+
+    parser.add_argument('file', help='file containing items to sort')
+    parser.add_argument('-a', '--algorithm', help='algorithm to sort with',
+                        choices=algorithms, default='merge')
+    parser.add_argument('-d', '--descending', '--reverse',
+                        help='order sequence in descending order', action='store_true')
+    parser.add_argument('-i', '--force-integers',
+                        help='parse lines from files as integers', action='store_true')
+
+    args = parser.parse_args()
+
+    if args.file is not None:
+        uData = _from_file(args.file, args.force_integers)
+        c = comptSort(uData, sort=args.algorithm, asc=not args.descending)
+        print(c)
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
